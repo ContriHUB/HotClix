f3583028781e183b2da9c506d1f7a3277f49a497
Dharmendra <78801686+ProgrammingPirates@users.noreply.github.com>
2021-10-09 15:12:07 +0530

    Create shareimage.js

---
+++ shareimage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import { NativeModules, Platform } from 'react-native';
+const ShareImages = NativeModules.ShareImages;
+
+var ShareImagesModule = Platform.web == 'web' ? {
+  show: ShareImages.show
+} : {
+  show(options: Object) {
+      ShareImages.show(options);
+    },
+    render() {
+     return (<View></View>);
+    }
+}
+
+module.exports = ShareImagesModule;
