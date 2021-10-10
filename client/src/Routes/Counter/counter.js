940f1eb68b0d64cd3e28481464f984c0c07668b3
Dharmendra <78801686+ProgrammingPirates@users.noreply.github.com>
2021-10-10 15:49:17 +0530

    i update my repo

--- client/src/Routes/Counter/counter.js
+++ client/src/Routes/Counter/counter.js
@@ -1,32 +1,101 @@
-<script>
-  var total_visitor_list = [10,10000,10000];
-  var visitor_cnt_0 = total_visitor_list[0];
-  var visitor_cnt_1 = total_visitor_list[1];
-  var visitor_cnt_2 = total_visitor_list[2];
-  var result_data = "";
-  var display_div = document.getElementById("display_div_id");
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-  function CounterVisitor(current_count){
-    setInterval(function(){
-      // clear count
-      while (display_div.hasChildNodes()) {
-          display_div.removeChild(display_div.lastChild);
-      }
-      visitor_cnt_0++;
-      if (visitor_cnt_0 > 99) {
-        visitor_cnt_0 = 10; // reset count
-        visitor_cnt_1++;    // increase next count
-      }
-      if(visitor_cnt_1>99999){
-        visitor_cnt_2++;
-      }
-      result_data = visitor_cnt_2.toString() + visitor_cnt_1.toString() + visitor_cnt_0.toString();
-      for (var i = 0; i < result_data.length; i++) {
-        var live_flag = document.createElement('span');
-        live_flag.className = 'live_cnt';
-        live_flag.innerText = result_data[i];
-        display_div.appendChild(live_flag);
-      }
-    },1000);
+const Card = ({ image }) => {
+  const tags = image.tags.split(",");
+
+  return (
+    <div css={styles} className="card">
+      <a href={image.largeImageURL} target="_blank" rel="noopener noreferrer">
+        <img src={image.webformatURL} alt='photograph' className="cardImage" />
+        <div className="cardInfo">
+          <p className="user">Photo by {image.user}</p>
+          <p>
+            <i className="fas fa-eye"></i>
+            <span>Views:</span>
+            {image.views}
+          </p>
+          <p>
+            <i className="fas fa-download"></i>
+            <span>Downloads:</span>
+            {image.downloads}
+          </p>
+          <p>
+            <i className="fas fa-heart"></i>
+            <span>Likes:</span>
+            {image.likes}
+          </p>
+          <p>
+            <i className="fas fa-comment"></i>
+            <span>Comments:</span>
+            {image.comments}
+          </p>
+          <div className="tags">
+            {tags.map((tag, index) =>
+              tag.length >= 11 ? "" : <span key={index}>#{tag}</span>
+            )}
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+const styles = css`
+  width: 100%;
+  max-width: 360px;
+  background: #f5f5f5;
+  margin: 12px 0;
+  box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0, 0, 0, 0);
+  a {
+    text-decoration: none;
   }
-</script>
+  .cardImage {
+    width: 100%;
+    height: 240px;
+    display: block;
+  }
+  .cardInfo {
+    padding: 14px 20px 20px 20px;
+    p {
+      font-size: 17px;
+      font-weight: 500;
+      color: #364f6b;
+      margin: 8px 10px;
+      i,
+      span {
+        color: #fc5185;
+      }
+      span {
+        margin: 0 4px 0 12px;
+      }
+      &.user {
+        color: #fc5185;
+        font-size: 22px;
+        font-weight: 700;
+      }
+    }
+    .tags {
+      display: flex;
+      align-items: center;
+      span {
+        margin: 8px 10px 0 0;
+        background: #fc5185;
+        color: #f5f5f5;
+        border-radius: 50px;
+        padding: 6px;
+        font-size: 14px;
+        width: 100px;
+        text-align: center;
+      }
+    }
+  }
+  @media (max-width: 839px) {
+    max-width: 100%;
+  }
+  @media (min-width: 840px) and (max-width: 1180px) {
+    max-width: 48%;
+  }
+`;
+
+export default Card;
